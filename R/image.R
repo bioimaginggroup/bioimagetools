c17f1ecc1274512d02a95e70fabe15eb1b381262
ua341fy <volker.schmid@stat.uni-muenchen.de>
2015-09-16 16:28:29 +0200

    image.rgb added readTIF better support for 3d rgb
    Former-commit-id: e0f370452c070874a94d49d9849bc38cbe0e2aaa

--- R/image.R
+++ R/image.R
@@ -8,12 +8,28 @@
 #' @export
 image<-function(x,col="grey",...)
 {
+  if(length(dim(x))==3)image.rgb(x,...)
   a<-seq(0,1,length=1000)
   b=rep(0,1000)
   if(col=="grey")colo=grey(a)
   if(col=="red"|col=="r")colo=rgb(a,b,b)
   if(col=="green"|col=="g")colo=rgb(b,a,b)
   if(col=="blue"|col=="b")colo=rgb(b,b,a)
+  x<-aperm(x,c(2,1))
+  x[,dim(x)[2]:1]<-x
   graphics::image(1:dim(x)[1],1:dim(x)[2],x,axes=FALSE,col=colo,xlab="",ylab="",...)
 }
 
+image.rgb<-function(x,n=100,...)
+{
+  n<-100
+  a<-b<-c<-seq(0,1,length=100)
+  a<-rep(a,each=n*n)
+  b<-rep(rep(b,each=n),times=n)
+  c<-rep(c,times=n*n)
+  colo=rgb(a,b,c)
+  xx<-floor(1+x[,,1]*(n-1))*n*n+floor(1+x[,,2]*(n-1))*n+floor(1+x[,,3]*(n-1))
+  xx<-aperm(xx,c(2,1))
+  xx[,dim(xx)[2]:1]<-xx
+  graphics::image(1:dim(x)[2],1:dim(x)[1],xx,axes=FALSE,col=colo,xlab="",ylab="",zlim=c(1,n*n*n),...)
+}
--- R/readtif.R
+++ R/readtif.R
@@ -11,31 +11,31 @@ readTIF<-function(file=file.choose(),native=FALSE,as.is=FALSE,channels=NULL)
   require(tiff)
   li<-readTIFF(file,all=TRUE,info=TRUE,as.is=as.is,native=native)
   Z<-length(li)
-  img<-array(0,c(dim(li[[1]])[1:2],Z))
+  img<-array(0,c(dim(li[[1]]),Z))
   if(length(dim(li[[1]]))==2)for (i in 1:Z)img[,,i]<-li[[i]]
-  if(length(dim(li[[1]]))==3)for (i in 1:Z)img[,,i]<-li[[i]][,,1]
+  if(length(dim(li[[1]]))==3)for (i in 1:Z)img[,,,i]<-li[[i]]
   #storage.mode(img)<-"integer"
   temp<-attributes(li[[1]])
   tmp<-gregexpr("\n",temp$description)
   if(length(tmp)>0)if (tmp[[1]][1]!=-1)
+  {
+    temp2<-regmatches(temp$description,tmp,invert=TRUE)[[1]]
+    temp3<-temp4<-c()
+    for (i in temp2)
     {
-      temp2<-regmatches(temp$description,tmp,invert=TRUE)[[1]]
-      temp3<-temp4<-c()
-      for (i in temp2)
-      {
-        j<-gregexpr("=",i)[[1]]
-        j<-regmatches(i,j,invert=TRUE)[[1]]
-        temp3<-c(temp3,j[1])
-        temp4<-c(temp4,j[2])
-      }
-      names(temp4)<-temp3
-      temp<-c(temp,temp4)
+      j<-gregexpr("=",i)[[1]]
+      j<-regmatches(i,j,invert=TRUE)[[1]]
+      temp3<-c(temp3,j[1])
+      temp4<-c(temp4,j[2])
+    }
+    names(temp4)<-temp3
+    temp<-c(temp,temp4)
   }
   temp<-temp[!(names(temp)=="")]
   K<-as.integer(temp$channels)
   if (!is.null(channels)){K<-channels}
   if(length(K)==0)K<-1
-  if (K>1)
+  if ((K>1)&(length(dim(img))<4))
   {
     img<-array(img,c(dim(li[[1]])[1:2],K,Z/K))
     #storage.mode(img)<-"integer"
@@ -45,7 +45,7 @@ readTIF<-function(file=file.choose(),native=FALSE,as.is=FALSE,channels=NULL)
   #if(min(img)<0){require(bitops);img<-array(bitFlip(img,bitWidth=temp$bits.per.sample),dim(img))}
   #img<-img/(2^(temp$bits.per.sample))
   if (!as.is)
-    {
+  {
     if (min(img)<0)img=img-min(img)
     if (max(img)>1)img<-img/max(img)
   }
