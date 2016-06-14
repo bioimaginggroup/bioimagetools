01e57eebbeb29faea5afbbdcfce1001410352ecb
Volker Schmid <volker.schmid@stat.uni-muenchen.de>
2016-06-14 10:49:20 +0200

    img() min und max
    Former-commit-id: dd26e6deff1a334dfdc87eac3324ac954d29979a

--- R/image.R
+++ R/image.R
@@ -2,11 +2,13 @@
 #'
 #' @param x Image, 2D or 3D Matrix
 #' @param col Color: "grey", "red" ("r"), "green" ("g") or "blue" ("b"), "rgb" for 3D matrices
+#' @param min minimal value of shown intensity
+#' @param max maximal value of shown intensity
 #' @param ... other parameters for graphics::image
 #'
 #' @return no return
 #' @export
-img<-function(x,col="grey",...)
+img<-function(x,col="grey",min=0,max=ifelse(dim(x)==2,max(x),apply(x,3,max)),...)
 {
   if(col=="rgb"){img.rgb(x,...);return()}
   a<-seq(0,1,length=1000)
@@ -17,7 +19,11 @@ img<-function(x,col="grey",...)
   if(col=="blue"|col=="b")colo=rgb(b,b,a)
   x<-aperm(x,c(2,1))
   x[,dim(x)[2]:1]<-x
-  graphics::image(1:dim(x)[1],1:dim(x)[2],x,axes=FALSE,col=colo,xlab="",ylab="",...)
+  x<-x-min
+  x=x/(max-min)
+  x[x<0]<-0
+  x[x>1]<-1
+  graphics::image(1:dim(x)[1],1:dim(x)[2],x,axes=FALSE,col=colo,xlab="",ylab="", zlim=c(0,1),...)
 }
 
 img.rgb<-function(x,n=100,...)
@@ -28,8 +34,18 @@ img.rgb<-function(x,n=100,...)
   b<-rep(rep(b,each=n),times=n)
   c<-rep(c,times=n*n)
   colo=rgb(a,b,c)
+  if (length(min)==1)x=x-min
+  if (length(min)==3)for (i in 1:3)x[,,i]=x[,,i]-min[i]
+  if (length(max)==1&length(min)==1)x=x/(max-min)
+  if (length(max)==3&length(min)==3)for (i in 1:3)x[,,i]=x[,,i]/(max[i]-min[i])
+  if (length(max)==1&length(min)==3)for (i in 1:3)x[,,i]=x[,,i]/(max-min[i])
+  if (length(max)==3&length(min)==1)for (i in 1:3)x[,,i]=x[,,i]/(max[i]-min)
+  x[x<0]<-0
+  x[x>1]<-1
+  
   xx<-floor(1+x[,,1]*(n-1))*n*n+floor(1+x[,,2]*(n-1))*n+floor(1+x[,,3]*(n-1))
   xx<-aperm(xx,c(2,1))
   xx[,dim(xx)[2]:1]<-xx
+  
   graphics::image(1:dim(x)[2],1:dim(x)[1],xx,axes=FALSE,col=colo,xlab="",ylab="",zlim=c(1,n*n*n),...)
 }
