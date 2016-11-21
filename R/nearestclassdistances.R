d746512206a92378031251559d50bdc7bf35bb95
Volker Schmid <volker.schmid@stat.uni-muenchen.de>
2016-11-21 23:24:41 +0100

    cores
    Former-commit-id: 471e9732140cb084594d5f9de75c55c272012e23

--- R/nearestclassdistances.R
+++ R/nearestclassdistances.R
@@ -25,7 +25,7 @@ nearestClassDistances<-function(img,voxelsize,classes=7,silent=FALSE,cores=1,alg
       ww<-as.matrix(which(img==class,arr.ind = TRUE))
       n<-dim(ww)[1]
       dist<-vector(mode="numeric",length=n)
-      if(cores>1)alist[[class]]<-mclapply(1:classes,nearestClassDistancesClass,dist,ww,zscale,n,img)
+      if(cores>1)alist[[class]]<-mclapply(1:classes,nearestClassDistancesClass,dist,ww,zscale,n,img, mc.cores=cores)
       if(cores==1)alist[[class]]<-lapply(1:classes,nearestClassDistancesClass,dist,ww,zscale,n,img)
     }
   }
